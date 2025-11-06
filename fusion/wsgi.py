f1491a0fd0a6e1222af64ebd866149f39fbb6f03
sergio <sjunior1557@gmail.com>
2025-11-06 16:17:42 -0300

    debug: print traceback no wsgi

--- fusion/wsgi.py
+++ fusion/wsgi.py
@@ -1,18 +1,17 @@
-"""
-WSGI config for fusion project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+import traceback
 
-from django.core.wsgi import get_wsgi_application
+print(">>> Iniciando WSGI...", file=sys.stderr)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fusion.settings')
 
-application = get_wsgi_application()
-
-    
+try:
+    from django.core.wsgi import get_wsgi_application
+    application = get_wsgi_application()
+    print("✅ Django carregado com sucesso!", file=sys.stderr)
+except Exception:
+    print("❌ Erro ao iniciar Django:", file=sys.stderr)
+    traceback.print_exc(file=sys.stderr)
+    # Rethrow o erro pra Vercel registrar o 500
+    raise
